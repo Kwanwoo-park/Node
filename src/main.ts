8abdda70103bed6400098256d2f7187719e3c3f9
akakslslzz@naver.com <akakslslzz@naver.com>
2026-02-07 23:55:25 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:8081',
     credentials: true,
   })
 
